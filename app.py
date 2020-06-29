430559779d3e7ef12a3a92c9f091c179ed82903a
Sahil Chachra <37156032+SahilChachra@users.noreply.github.com>
2020-06-29 18:14:10 +0530

    Completed Deployment

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, url_for, redirect, render_template
+import pickle
+
+import numpy as np
+
+app = Flask(__name__, template_folder='./templates', static_folder='./static')
+
+Pkl_Filename = "rf_tuned.pkl" 
+with open(Pkl_Filename, 'rb') as file:  
+    model = pickle.load(file)
+@app.route('/')
+
+def hello_world():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST','GET'])
+def predict():
+    features = [int(x) for x in request.form.values()]
+
+    print(features)
+    final = np.array(features).reshape((1,6))
+    #print(type(final))
+    print(final)
+    #print(final.shape)
+    pred = model.predict(final)[0]
+    print(pred)
+    #print(pred[0][0])
+    
+    if pred < 0:
+        return render_template('home.html', pred='Error calculating Amount!')
+    else:
+        return render_template('home.html', pred='Expected amount is {0:.3f}'.format(pred))
+
+if __name__ == '__main__':
+    app.run(debug=True)
